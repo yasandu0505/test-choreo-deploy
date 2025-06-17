cf8811214bd50d42bdb4fcf639055630b867f600
yasandu0505 <yasanduyaka2005@gmail.com>
2025-06-17 16:47:40 +0530

    fix: resolve deploy errors

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,11 @@ function App() {
   useEffect(() => {
     const fetchMinistries = async () => {
       try {
-        const response = await fetch("/v1/entities/2153-12_min_1/relations", {
-          method: "POST",
+        const response = await fetch("/books", {
+          method: "GET",
           headers: {
             "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            relatedEntityId: "",
-            startTime: "2019-12-10T00:00:00Z",
-            endTime: "",
-            id: "",
-            name: "AS_DEPARTMENT",
-          }),
+          }
         });
 
         if (!response.ok) {
